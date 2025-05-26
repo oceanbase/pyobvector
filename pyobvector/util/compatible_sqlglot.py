0c04dbaadcf494c2df8fc6dda6d3413504313a15
He Wang <wanghechn@qq.com>
2025-05-26 21:37:16 +0800

    fix typo

--- pyobvector/util/compatible_sqlglot.py
+++ pyobvector/util/compatible_sqlglot.py
@@ -4,7 +4,7 @@ from sqlglot import parse_one as _parse_one
 
 
 def parse_one(sql: str, dialect = "oceanbase"):
-    signature = inspect.signature(parse_one)
+    signature = inspect.signature(_parse_one)
     if 'dialect' in signature.parameters:
         return _parse_one(sql=sql, dialect=dialect)
     return _parse_one(sql=sql)
