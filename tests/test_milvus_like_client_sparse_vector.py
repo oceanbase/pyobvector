5f388b3aa7a46bc2b974d65a6128c2c735e2a058
shanhaikang.shk <shanhaikang.shk@oceanbase.com>
2025-09-08 11:49:10 +0800

    add sparse_vector_calculation & imilarity_search_with_scalar_filter tests
    Signed-off-by: shanhaikang.shk <shanhaikang.shk@oceanbase.com>

--- tests/test_milvus_like_client_sparse_vector.py
+++ tests/test_milvus_like_client_sparse_vector.py
@@ -87,8 +87,18 @@ class ObMilkClientTestSparseVector(unittest.TestCase):
         schema.add_field(field_name="sparse_vector", datatype=DataType.SPARSE_FLOAT_VECTOR)
         schema.add_field(field_name="text", datatype=DataType.VARCHAR, max_length=65535)
 
+        idx_params = self.client.prepare_index_params()
+        idx_params.add_index(
+            field_name="sparse_vector",
+            index_type="daat",
+            index_name="spiv_on_t2",
+            metric_type="inner_product",
+        )
+
         self.client.create_collection(
-            collection_name=collection_name, schema=schema,
+            collection_name=collection_name,
+            schema=schema,
+            index_params=idx_params,
         )
 
         data = [
@@ -124,5 +134,139 @@ class ObMilkClientTestSparseVector(unittest.TestCase):
             [
                 {'pk': 3, 'text': 'Hello, world!', 'sparse_vector': '{1:0.7,50:0.7,1000:0.9}'},
                 {'pk': 2, 'text': 'information retrieval focuses on finding relevant information in large datasets.', 'sparse_vector': '{10:0.1,200:0.7,1000:0.9}'}
+            ]
+        )
+    
+    def test_sparse_vector_distance_calculation(self):
+        collection_name = "sparse_vector_distance_calculation"
+        self.client.drop_collection(collection_name)
+
+        schema = self.client.create_schema()
+        
+        schema.add_field(field_name="pk", datatype=DataType.INT32, is_primary=True, auto_id=True)
+        schema.add_field(field_name="c2", datatype=DataType.SPARSE_FLOAT_VECTOR)
+        schema.add_field(field_name="c3", datatype=DataType.SPARSE_FLOAT_VECTOR)
+
+        self.client.create_collection(
+            collection_name=collection_name, schema=schema,
+        )
+
+        data = [
+            {
+                "c2": {1:1.1, 2:2.2},
+                "c3": {1:2.4},
+            },
+            {
+                "c2": {1:1.5, 3:3.6},
+                "c3": {4:4.5},
+            }
+        ]
+        self.client.insert(
+            collection_name=collection_name,
+            data=data,
+        )
+
+        res = self.client.perform_raw_text_sql(
+            f"select inner_product(c2,c3) from {collection_name}"
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [(2.640000104904175,), (0.0,)]
+        )
+
+        res = self.client.perform_raw_text_sql(
+            f"select negative_inner_product(c2,c3) from {collection_name}"
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [(-2.640000104904175,), (0.0,)]
+        )
+
+        query = {1:2.4}
+        res = self.client.perform_raw_text_sql(
+            f"select inner_product(c2,'{query}') from {collection_name}"
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [(2.640000104904175,), (3.6000001430511475,)]
+        )
+
+    def test_similarity_search_with_scalar_filter(self):
+        collection_name = "similarity_search_with_scalar_filter"
+        self.client.drop_collection(collection_name)
+
+        schema = self.client.create_schema()
+
+        schema.add_field(field_name="pk", datatype=DataType.INT32, is_primary=True, auto_id=True)
+        schema.add_field(field_name="c1", datatype=DataType.SPARSE_FLOAT_VECTOR)
+        schema.add_field(field_name="c2", datatype=DataType.INT32)
+
+        idx_params = self.client.prepare_index_params()
+        idx_params.add_index(
+            field_name="c1",
+            index_type="daat",
+            index_name="spiv",
+            metric_type="inner_product",
+        )
+
+        self.client.create_collection(
+            collection_name=collection_name,
+            schema=schema,
+            index_params=idx_params,
+        )
+
+        data = [
+            {
+                "c1": {1: 0.5, 100: 0.3, 500: 0.8},
+                "c2": 10,
+            },
+            {
+                "c1": {10: 0.1, 200: 0.7, 1000: 0.9},
+                "c2": 4,
+            },
+            {
+                "c1": {1: 0.7, 50: 0.7, 1000: 0.9},
+                "c2": 1,
+            }
+        ]
+        self.client.insert(
+            collection_name=collection_name,
+            data=data,
+        )
+
+        query_data = {1: 0.2, 50: 0.4, 1000: 0.7}
+        res = self.client.search(
+            collection_name=collection_name,
+            data=query_data,
+            anns_field="c1",
+            limit=2,
+            output_fields=["pk", "c1", "c2"],
+            search_params={"metric_type": "neg_ip"},
+            flter=[text("c2 >= 4")]
+        )
+        self.assertEqual(
+            res,
+            [
+                {'pk': 2, 'c1': '{10:0.1,200:0.7,1000:0.9}', 'c2': 4},
+                {'pk': 1, 'c1': '{1:0.5,100:0.3,500:0.8}', 'c2': 10}
+            ]
+        )
+
+        # You can also use SQLAlchemy.Table
+        table = self.client.load_table(collection_name=collection_name)
+        res = self.client.search(
+            collection_name=collection_name,
+            data=query_data,
+            anns_field="c1",
+            limit=2,
+            output_fields=["pk", "c1", "c2"],
+            search_params={"metric_type": "neg_ip"},
+            flter=[table.c["c2"] >= 4]
+        )
+        self.assertEqual(
+            res,
+            [
+                {'pk': 2, 'c1': '{10:0.1,200:0.7,1000:0.9}', 'c2': 4},
+                {'pk': 1, 'c1': '{1:0.5,100:0.3,500:0.8}', 'c2': 10}
             ]
         )
--- tests/test_ob_vec_client_sparse_vector.py
+++ tests/test_ob_vec_client_sparse_vector.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 from pyobvector import *
-from sqlalchemy import Column, Integer, JSON, String, text
+from sqlalchemy import Column, Integer, JSON, String, text, Table
 from sqlalchemy import func
 import logging
 
@@ -145,5 +145,141 @@ class ObVecClientTestSparseVector(unittest.TestCase):
             [
                 (3, 'Hello, world!', '{1:0.7,50:0.7,1000:0.9}'),
                 (2, 'information retrieval focuses on finding relevant information in large datasets.', '{10:0.1,200:0.7,1000:0.9}')
+            ]
+        )
+
+    def test_sparse_vector_distance_calculation(self):
+        table_name = "obvec_sparse_vector_distance_calculation"
+        self.client.drop_table_if_exist(table_name)
+
+        cols = [
+            Column("pk", Integer, primary_key=True, autoincrement=True),
+            Column("c2", SPARSE_VECTOR),
+            Column("c3", SPARSE_VECTOR),
+        ]
+
+        self.client.create_table(
+            table_name=table_name,
+            columns=cols,
+        )
+
+        data = [
+            {
+                "c2": {1:1.1, 2:2.2},
+                "c3": {1:2.4},
+            },
+            {
+                "c2": {1:1.5, 3:3.6},
+                "c3": {4:4.5},
+            }
+        ]
+        self.client.insert(
+            table_name=table_name,
+            data=data,
+        )
+
+        res = self.client.perform_raw_text_sql(
+            f"select inner_product(c2,c3) from {table_name}"
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [(2.640000104904175,), (0.0,)]
+        )
+
+        res = self.client.perform_raw_text_sql(
+            f"select negative_inner_product(c2,c3) from {table_name}"
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [(-2.640000104904175,), (0.0,)]
+        )
+
+        query = {1:2.4}
+        res = self.client.perform_raw_text_sql(
+            f"select inner_product(c2,'{query}') from {table_name}"
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [(2.640000104904175,), (3.6000001430511475,)]
+        )
+
+    def test_similarity_search_with_scalar_filter(self):
+        table_name = "obvec_similarity_search_with_scalar_filter"
+        self.client.drop_table_if_exist(table_name)
+
+        cols = [
+            Column("pk", Integer, primary_key=True, autoincrement=True),
+            Column("c1", SPARSE_VECTOR),
+            Column("c2", Integer),
+        ]
+
+        idx_params = self.client.prepare_index_params()
+        idx_params.add_index(
+            field_name="c1",
+            index_type="daat",
+            index_name="spiv",
+            metric_type="inner_product",
+        )
+        self.client.create_table_with_index_params(
+            table_name=table_name,
+            columns=cols,
+            vidxs=idx_params,
+        )
+
+        data = [
+            {
+                "c1": {1: 0.5, 100: 0.3, 500: 0.8},
+                "c2": 10,
+            },
+            {
+                "c1": {10: 0.1, 200: 0.7, 1000: 0.9},
+                "c2": 4,
+            },
+            {
+                "c1": {1: 0.7, 50: 0.7, 1000: 0.9},
+                "c2": 1,
+            }
+        ]
+        self.client.insert(
+            table_name=table_name,
+            data=data,
+        )
+
+        query_data = {1: 0.2, 50: 0.4, 1000: 0.7}
+        res = self.client.ann_search(
+            table_name=table_name,
+            vec_data=query_data,
+            vec_column_name="c1",
+            distance_func=negative_inner_product,
+            with_dist=False,
+            topk=2,
+            output_column_names=["pk", "c1", "c2"],
+            where_clause=[text("c2 >= 4")],
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [
+                (2, '{10:0.1,200:0.7,1000:0.9}', 4),
+                (1, '{1:0.5,100:0.3,500:0.8}', 10)
+            ]
+        )
+
+        # You can also use SQLAlchemy.Table
+        table = Table(table_name, self.client.metadata_obj, autoload_with=self.client.engine)
+        res = self.client.ann_search(
+            table_name=table_name,
+            vec_data=query_data,
+            vec_column_name="c1",
+            distance_func=negative_inner_product,
+            with_dist=False,
+            topk=2,
+            output_column_names=["pk", "c1", "c2"],
+            where_clause=[table.c["c2"] >= 4]
+        )
+        self.assertEqual(
+            res.fetchall(),
+            [
+                (2, '{10:0.1,200:0.7,1000:0.9}', 4),
+                (1, '{1:0.5,100:0.3,500:0.8}', 10)
             ]
         )
